3ebeffd12e922549e02fbe86ef6bb70b08733d0c
adil <erradilyo@gmail.com>
2022-03-05 00:14:14 +0100

    Add : "unplugin-vue-components" auto load components on demand

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   resolve: {
@@ -8,5 +9,5 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), Components()],
 })
